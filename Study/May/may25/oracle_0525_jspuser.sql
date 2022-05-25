4ad69e85d2237b4b89b52b4499c94bfae0793ac7
YUL\sksrk <ckdlsktkdgus@naver.com>
2022-05-25 18:02:10 +0900

    5.25 학원 공부 오라클 쿼리연습과 mybatis 세팅 시작

---
+++ Study/May/may25/oracle_0525_jspuser.sql
@@ -0,0 +1,21 @@
+
+-- 메모장 테이블 만들기
+-- 시퀀스부터 만든다.
+   CREATE SEQUENCE memo_idx_seq;
+-- 테이블을 만들자
+   CREATE TABLE memo(
+  		idx NUMBER PRIMARY KEY,
+  		name varchar2(100) NOT NULL,
+  		password varchar2(100) NOT NULL,
+  		content varchar2(100) NOT NULL,
+  		regdate timestamp DEFAULT SYSDATE,
+  		ip varchar2(100) NOT NULL
+   );
+  
+  INSERT INTO 
+  	memo (idx,name,password,content,ip) 
+  VALUES
+  	(memo_idx_seq.nextval,'한사람','1234','와~~~ 1빠다!!!','192.168.0.124');
+  
+  SELECT * FROM memo;
+  
