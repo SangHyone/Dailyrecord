f1e6c1040cc1dc2dbea3971a40b92ae0fbe9e235
YUL\sksrk <ckdlsktkdgus@naver.com>
2022-07-05 18:09:46 +0900

    유클리드 호제법 사용.

--- Programers/lv2/LeastCommonMultiple.java
+++ Programers/lv2/LeastCommonMultiple.java
@@ -8,12 +8,10 @@ public class LeastCommonMultiple {
     
     public int solution(int[] arr) {
         int answer = arr[0];
-        
         for(int i = 1;i<arr.length;i++){
             int gcd = gcd(answer,arr[i]);
             answer = answer * arr[i] / gcd;
         }
-        
         return answer;
     }
 }
